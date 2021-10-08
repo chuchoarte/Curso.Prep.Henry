d1d6cc97edb62e06cc9d8449edd64bc1968e03f9
Jesus <surjesus.sistemas@gmail.com>
2021-10-08 02:16:06 -0500

    16 passed

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -147,8 +147,8 @@ function empiezaConNueve(n) {
 function todosIguales(arreglo) {
   //Escriba la función todosIguales, que indique si todos los elementos de un arreglo son iguales:
   //retornar true, caso contrario retornar false.
-  //Escribe tu código aquí  
-  for (let i =0 ; i < arreglo.length - 1; i++) {
+  //Escribe tu código aquí
+  for (let i = 0; i < arreglo.length - 1; i++) {
     return arreglo[i] !== arreglo[i+1] ? false : true;
   }
 } 
